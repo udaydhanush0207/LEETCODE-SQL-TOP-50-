2fb8139f4f24cad3f1659acaa8b6f3f9b2436bd0
Venkat Satya Uday Dhanush Karri <nie24@txstate.edu>
2025-09-29 21:51:32 +0000

    [LeetCode Sync] Runtime - 492 ms (67.00%), Memory - 0.0B (100.00%)

---
+++ problems/1161-project-employees-i/solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select a.project_id, Round(avg(b.experience_years),2) as average_years
+from Project a 
+left join Employee b
+on a.employee_id = b.employee_id
+group by a.project_id
